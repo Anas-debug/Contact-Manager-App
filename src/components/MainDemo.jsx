f6475e7227e1c43ee9018d209f64730a904e39be
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-04 23:09:57 +0100

    MainDemo component setup

--- src/components/MainDemo.jsx
+++ src/components/MainDemo.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+import { ReactDOM } from 'react';
+import AddPersonFrom from './AddPersonForm';
+import ContactManager from './ContactManager';
+import PeopleList from './PeopleList';
+
+// Fake data
+const contacts = ["Daniel", "Omar", "Amir", "Joseph", "Mary"];
+
+const el = (
+    <div>
+        <AddPersonFrom />
+        <PeopleList data = {contacts} />
+    </div>
+);
+
+ReactDOM.render(el , document.getElementById('root'));
