f3d0fc0745cd587374240e2583e35b63a3cc077e
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-04 23:17:12 +0100

    Test the App with MainDemo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
+import MainDemo from './components/MainDemo';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <MainDemo />
   );
 }
 
--- src/components/MainDemo.jsx
+++ src/components/MainDemo.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { ReactDOM } from 'react';
 import AddPersonFrom from './AddPersonForm';
-import ContactManager from './ContactManager';
 import PeopleList from './PeopleList';
 
 // Fake data
 const contacts = ["Daniel", "Omar", "Amir", "Joseph", "Mary"];
 
-const el = (
-    <div>
-        <AddPersonFrom />
-        <PeopleList data = {contacts} />
-    </div>
-);
 
-ReactDOM.render(el , document.getElementById('root'));
+
+
+export default function MainDemo(){
+    return(
+        <div>
+            <AddPersonFrom />
+            <PeopleList data = {contacts} />
+        </div>
+    );
+}
